921328e7b1d07dbc551fd166ef001988e4bffe5e
mfronheiser <129707410+mfronheiser@users.noreply.github.com>
2024-01-20 16:16:23 -0500

    Revert "Mf dev"

--- src/components/search.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { withRouter } from "react-router-dom";
-
-const SearchPage = (props) => {
-  return <h1>Here is the search page</h1>;
-};
-
-export default withRouter(SearchPage);
--- src/components/wishlist.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { withRouter } from "react-router-dom";
-
-const WishList = (props) => {
-  return <h1>Here is the wishlist page</h1>;
-};
-
-export default withRouter(WishList);
