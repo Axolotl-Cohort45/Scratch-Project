f240d44850f53dd845f7b553a277bb41de2d4ade
Dillon <dillonhale2026@gmail.com>
2024-01-20 16:13:57 -0500

    NeedToFleshOutPOSTs

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,39 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return (
     <header>
-      <div className="container">
-        <div className="inner-content">
-          <div className="brand">
+      <div className='container'>
+        <div className='inner-content'>
+          <div className='brand'>
             <img
-              src={require("../img/cutlery.png").default}
-              width="50"
-              height="50"
+              src={require('../img/cutlery.png').default}
+              width='50'
+              height='50'
             ></img>
-            <Link to="/">Restaurant-Buddy</Link>
+            <Link to='/'>Restaurant-Buddy</Link>
           </div>
-          <ul className="nav-links">
+          <ul className='nav-links'>
             <li>
-              <Link to="/" className="btn btn-main">
+              <Link to='/' className='btn btn-main'>
                 My FootPrints
               </Link>
             </li>
             <li>
-              <Link to="/wishlist" className="btn btn-main">
+              <Link to='/wishlist' className='btn btn-main'>
                 Wish Lists
               </Link>
             </li>
             <li>
-              <Link to="/search-new" className="btn btn-main">
+              <Link to='/search-new' className='btn btn-main'>
                 Explore New
+              </Link>
+            </li>
+            {/* Remember to Remove After! */}
+            <li>
+              <Link to="/brew-details" className="btn btn-main">
+                Test Brewery Page Link
               </Link>
             </li>
           </ul>
