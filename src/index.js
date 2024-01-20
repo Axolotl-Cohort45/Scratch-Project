6c8671c1e847f030f0f760a91cbedbf51079157a
Eliza612 <yameng612@gmail.com>
2024-01-19 21:14:35 -0500

    add header

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
---
+++ src/redux/reducers/reducers.js
@@ -0,0 +1,24 @@
+import * as types from "../actionTypes";
+
+const initialState = {
+  wishList: [],
+};
+
+const Reducers = (state = initialState, action) => {
+  switch (action.type) {
+    case types.SEARCH_RESTAUANTS:
+
+    case types.SAVE_TO_WISHLIST:
+
+    case types.ADD_FOOTPRINT:
+
+    case types.EDIT_FOOTPRINT:
+
+    case types.DELETE_FOOTPRINT:
+
+    default:
+      return state;
+  }
+};
+
+export default Reducers;
