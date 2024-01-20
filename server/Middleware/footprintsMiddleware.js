2bbe923460a0336d9984a3b344169ef8aa880644
mfronheiser <meredith.fronheiser@gmail.com>
2024-01-20 16:13:06 -0500

    api to update db

---
+++ server/Middleware/footprintsMiddleware.js
@@ -0,0 +1,59 @@
+const footprintsMiddleware = {};
+
+footprintsMiddleware.getCityId = async (req, res, next) => {
+  //   const { cityName } = req.body;
+
+  const answer = {
+    cityName: 'Philadelphia',
+    // brewName: '2nd Story Brewing Company'
+  };
+
+  const citytestName = 'Philadelphia';
+
+  //   const cityUrl = `https://api.openbrewerydb.org/v1/breweries?by_city=${urlCityName}`;
+
+  //   const brewName = '2nd Story Brewing Company';
+
+  //   const brewUrl = `https://api.openbrewerydb.org/v1/breweries?by_name=${urlBrewName}`;
+
+  let url;
+
+  if (!answer.cityName) {
+    const urlBrewName = encodeURIComponent(answer.brewName);
+    url = `https://api.openbrewerydb.org/v1/breweries?by_name=${urlBrewName}`;
+  } else {
+    const urlCityName = encodeURIComponent(answer.cityName);
+    url = `https://api.openbrewerydb.org/v1/breweries?by_city=${urlCityName}`;
+  }
+
+  console.log(url);
+
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+
+    res.locals.result = result;
+
+    const brewList = result.map((data) => data.name);
+    const addressList = result.map((data) => data.address_1);
+    const cityList = result.map((data) => data.city);
+    const stateList = result.map((data) => data.state);
+    const phoneList = result.map((data) => data.phone);
+    const websiteList = result.map((data) => data.website_url);
+    res.locals.brewList = brewList;
+    res.locals.addressList = addressList;
+    res.locals.cityList = cityList;
+    res.locals.stateList = stateList;
+    res.locals.phoneList = phoneList;
+    res.locals.websiteList = websiteList;
+
+    console.log((res.locals.brewList = brewList));
+
+    // console.log('res.locals', res.locals.cityList)
+    return next();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = footprintsMiddleware;
--- server/models/wishlistModels.js
+++ server/models/wishlistModels.js
@@ -1,8 +1,22 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+// require('dotenv').config();
+
+const MONGO_URI =
+  'mongodb+srv://mfronheiser:scratch1@brew.wnjxaus.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(MONGO_URI, {
+      // options for the connect method to parse the URI
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // sets the name of the DB that our collections are part of
+      dbName: 'Brew',
+})
+  
+
 const Schema = mongoose.Schema;
 
 const wishlistSchema = new Schema({
-  userId: { type: String, default: "user1" },
+  userId: { type: String, default: 'user1' },
   restaurant_name: { type: String, required: true },
   address: { type: String, required: true },
   city: { type: String, required: true },
@@ -11,4 +25,5 @@ const wishlistSchema = new Schema({
   cuisine: { type: Array, default: [] },
 });
 
-module.exports = mongoose.model("WishLists", wishlistSchema);
+const Wishlist = mongoose.model('WishLists', wishlistSchema)
+module.exports = { Wishlist }
